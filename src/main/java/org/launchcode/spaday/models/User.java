80aa33f6c4aa140e8c1dd2caf1aba066ff3748c0
Darren LaCour <lacourd@icloud.com>
2023-05-08 19:24:19 -0500

    Completed validation studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,21 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Username is required!")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+    @Email
     private String email;
+    @NotBlank(message = "Password is required!")
+    @Size(min=6, message = "Password must be a minimum of 6 characters")
     private String password;
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
@@ -16,6 +28,11 @@ public class User {
         this.password = password;
     }
 
+    private void checkPassword() {
+        if ((this.password != null && this.verifyPassword != null) && (!this.password.equals(this.verifyPassword))) {
+                this.verifyPassword = null;
+            }
+    }
     public String getUsername() {
         return username;
     }
@@ -38,5 +55,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        this.checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        this.checkPassword();
     }
 }
